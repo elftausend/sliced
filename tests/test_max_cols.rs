9792b03529f041be42500fc3125f96fa73b5e9c1
elftausend <76885970+elftausend@users.noreply.github.com>
2023-02-21 07:25:27 +0100

    Add max_cols may grad test

--- tests/test_max_cols.rs
+++ tests/test_max_cols.rs
@@ -1,5 +1,32 @@
 #[cfg(feature = "cpu")]
 #[test]
-fn test_max_rows_cpu() {
+fn test_max_cols_cpu() {
+    use sliced::{CPU, Buffer, MaxColsMayGrad, BinaryOpsMayGrad};
 
+    let device = CPU::new();
+    let rhs = Buffer::from((&device, [1, 4, 2]));   
+
+    #[rustfmt::skip]
+    let lhs = Buffer::from((&device, 
+    [
+        -3, 2, 3, 1,
+        1, 5, -5, 4,
+        -9, -2, -4, -1
+    ]));
+
+    let max_cols = device.max_cols(3, 4, &lhs);
+    assert_eq!(&*max_cols, [3, 5, -1]);
+
+    let out = device.add(&max_cols, &rhs);
+    
+    out.backward();
+    
+    #[rustfmt::skip]
+    let expected = [
+        0, 0, 1, 0, 
+        0, 1, 0, 0, 
+        0, 0, 0, 1
+    ];
+
+    assert_eq!(&*out, expected);
 }
