020ce0f9601cd248f3e4b1c7814a8cc0c8a29de0
elftausend <76885970+elftausend@users.noreply.github.com>
2023-03-28 17:25:40 +0200

    Fix slice_col_op_grad_lhs, add unit test

--- src/ops2/col_op/grad/cpu.rs
+++ src/ops2/col_op/grad/cpu.rs
@@ -1,28 +1,108 @@
 use std::ops::{AddAssign, Mul};
 
+// TODO: Fix row_op_grad, row op only supports rhs for grad fn
 pub fn slice_col_op_grad_lhs<T>(
     cols: usize,
     lhs: &[T],
     rhs: &[T],
     lhs_grad: &mut [T],
-    rhs_grad: &mut [T],
     out_grad: &[T],
     lhs_grad_fn: impl Fn(T, T) -> T,
-    rhs_grad_fn: impl Fn(T) -> T,
 ) where
     T: Copy + AddAssign + Mul<Output = T>,
 {
-    let mut rhs_cycle = rhs.iter().cycle();
-    for ((lhs, lhs_grad), out_grad) in lhs.iter().zip(lhs_grad).zip(out_grad) {
-        let rhs = rhs_cycle.next().unwrap();
-        *lhs_grad = lhs_grad_fn(*lhs, *rhs) * *out_grad;
+    let mut rhs_iter = rhs.iter();
+    let mut rhs_val = rhs_iter.next().unwrap();
+    for (idx, ((lhs, lhs_grad), out_grad)) in lhs.iter().zip(lhs_grad).zip(out_grad).enumerate() {
+        if (idx + 1) % (cols + 1) == 0 {
+            rhs_val = rhs_iter.next().unwrap();
+        }
+
+        *lhs_grad += lhs_grad_fn(*lhs, *rhs_val) * *out_grad;
     }
+}
+
+pub fn slice_col_op_grad_rhs<T>(
+    cols: usize,
+    lhs: &[T],
+    rhs: &[T],
+    rhs_grad: &mut [T],
+    out_grad: &[T],
+    rhs_grad_fn: impl Fn(T, T) -> T,
+) where
+    T: Copy + AddAssign + Mul<Output = T>,
+{
+
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::{slice_col_op_grad_lhs, test_utils::roughly_equals, slice_col_op_grad_rhs};
+
     #[test]
     fn test_slice_col_op_grad_lhs_div() {
+        #[rustfmt::skip]
+        let lhs = [
+            1., 2., 3., 
+            4., 5., 6.
+        ];
 
+        let mut lhs_grad = [0.; 6];
+
+        let rhs = [-3., 2.];
+
+        slice_col_op_grad_lhs(
+            3,
+            &lhs,
+            &rhs,
+            &mut lhs_grad,
+            &[1.4, 2.5, 3.3, 4., 5., 6.],
+            |_, rhs| 1. / rhs,
+        );
+
+        roughly_equals(
+            &lhs_grad,
+            &[
+                1.4 * 1. / -3.,
+                2.5 * 1. / -3.,
+                3.3 * 1. / -3.,
+                4. * 1. / 2.,
+                5. * 1. / 2.,
+                6. * 1. / 2.,
+            ],
+        );
+    }
+
+    #[test]
+    fn test_slice_col_op_grad_rhs_div() {
+        #[rustfmt::skip]
+        let lhs = [
+            1., 2., 3., 
+            4., 5., 6.
+        ];
+
+
+        let rhs = [-3., 2.];
+
+        let mut rhs_grad = [0.; 2];
+
+        let gf = |lhs: f64, rhs: f64| lhs / -(rhs * rhs);
+
+        slice_col_op_grad_rhs(
+            3,
+            &lhs,
+            &rhs,
+            &mut rhs_grad,
+            &[1.4, 2.5, 3.3, 4., 5., 6.],
+            gf,
+        );
+
+        roughly_equals(
+            &rhs_grad,
+            &[
+                gf(1., -3.) * 1.4 + gf(2., -3.) * 2.5 + gf(3., -3.) * 3.3,
+                gf(4., 2.) * 4. + gf(5., 2.) * 5. + gf(6., 2.) * 6.,
+            ],
+        );
     }
 }
